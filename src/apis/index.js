270ee797c0cfbdc04c5e54a30f3ae1fcd14d240a
Jungwoo-Son <jungwoo9356@gmail.com>
2020-09-13 23:23:07 +0900

    [ADD] post /diary-book

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,18 @@
 const route = require('express').Router();
+const DiaryBookService = require('../services/DiaryBookService');
+const DiaryBookRepo = require('../repositories/DiaryBookRepo');
 
-// const DiaryBookRoute = require('./routes/diaryBook');
 
-// route.use('/diary-book', DiaryBookRoute);
+const diary_book_service = new DiaryBookService(DiaryBookRepo);
+
+route.post('/diary-book', async (req, res) => {
+    const user = req.get('Authentication');
+    const diary_name = req.body.name;
+
+    await diary_book_service.makeNewDiaryBook(user, diary_name);
+    res.send('');
+});
+
+
 
 module.exports = route;
